5d110abc97d0b708fa9af5cf498325ba38414316
Joonas <joonas.aapro@mainiotech.fi>
2026-08-10 12:17:43 +0300

    Upgrade to v0.31

--- config/assets.rb
+++ config/assets.rb
@@ -2,9 +2,9 @@
 
 base_path = File.expand_path("..", __dir__)
 
-Decidim::Webpacker.register_path("#{base_path}/app/packs")
-Decidim::Webpacker.register_entrypoints(
+Decidim::Shakapacker.register_path("#{base_path}/app/packs")
+Decidim::Shakapacker.register_entrypoints(
   decidim_insights: "#{base_path}/app/packs/entrypoints/decidim_insights.js",
   decidim_insights_area: "#{base_path}/app/packs/entrypoints/decidim_insights_area.js"
 )
-Decidim::Webpacker.register_stylesheet_import("stylesheets/decidim/insights/insights")
+Decidim::Shakapacker.register_stylesheet_import("stylesheets/decidim/insights/insights")
